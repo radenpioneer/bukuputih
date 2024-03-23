4a5999ef18d747bc153b36a93d89373bc22e39fd
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-03-23 20:13:47 +0000

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import vercel from "@astrojs/vercel/serverless";
+import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
+import keystatic from '@keystatic/astro'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "hybrid",
+  adapter: vercel(),
+  integrations: [react(), markdoc(), keystatic()]
+});
